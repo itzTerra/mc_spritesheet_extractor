242e336570ba01255dde3deb70fe3d988ab148dd
Michal Doležel <michaldolezel123@gmail.com>
2023-06-07 09:04:49 +0200

    Made code clearer

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from typing import Dict, List, Tuple
 from django_slugify import slugify
 
 
-# Read the coordinates from the text file
 def readCoords(path: str, item_name = "all", print_debug = False) -> List[Dict]:
+    """
+    Read the coordinates from the text file, based on name
+
+    item_name -- regex or "all", empty string "" defaults to all
+    """
+    
     coordinates = []
     with open(path, "r") as file:
         for line in file:
@@ -16,7 +21,7 @@ def readCoords(path: str, item_name = "all", print_debug = False) -> List[Dict]:
             
             parts = line.split("\t")
             name = parts[0]
-            if item_name != "all" and not re.match(item_name, name):
+            if (item_name != "all" or item_name != "") and not re.match(item_name, name):
                 continue
             
             params = {}
@@ -33,7 +38,7 @@ def readCoords(path: str, item_name = "all", print_debug = False) -> List[Dict]:
     return coordinates
 
 
-# Extract each image from the sprite sheet
+# Extract each image from the spritesheet
 def extractImages(spritesheet: Image.Image, coords: List[Dict]) -> Dict[str, Image.Image]:
     images = {}
     for coord in coords:
@@ -59,7 +64,6 @@ def recolorImages(images: Dict[str, Image.Image], tintColor: Tuple[int]) -> None
                     pixels[i, j] = tintColor
     
 
-
 def saveImages(images: Dict[str, Image.Image]) -> None:
     for name, image in images.items():
         # Save the extracted image
@@ -72,13 +76,15 @@ def saveImages(images: Dict[str, Image.Image]) -> None:
 if __name__ == "__main__":
     SPRITESHEET_PATH = "input/minecraft_textures_atlas_particles.png_0.png"
     COORDS_PATH = "input/minecraft_textures_atlas_particles.png.txt"
+    
+    COORDS_NAME = r"minecraft:glitter_[0-9]+"
 
     TEAL = (106, 210, 178)
 
     # Load the sprite sheet image
     spritesheet = Image.open(SPRITESHEET_PATH)
 
-    coordinates = readCoords(COORDS_PATH, r"minecraft:glitter_[0-9]+")
+    coordinates = readCoords(COORDS_PATH, COORDS_NAME)
     images = extractImages(spritesheet, coordinates)
     recolorImages(images, TEAL)
     saveImages(images)
